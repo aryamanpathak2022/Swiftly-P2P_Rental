924d9d479b5c6265ad71ef8bd6a2328e25f3d949
aryamanpathak2022 <Aryaman.Pathak@iiitb.ac.in>
2024-06-14 23:31:51 +0530

    added google sign up

---
+++ backend_swiftly/backend_swiftly/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('user/', include('user.urls')),
+    path('api-auth/', include('rest_framework.urls')),  # Optional: Enables login for browsable API
+    path('accounts/', include('allauth.urls')),
+]
---
+++ backend_swiftly/user/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+from .views import register_user
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', register_user, name='register_user'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('example/', views.example_view, name='example'),
+    
+]
