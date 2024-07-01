0ed3ffbc6cd44f51b1c00585dedf5456d6c8f27c
aryamanpathak2022 <Aryaman.Pathak@iiitb.ac.in>
2024-07-01 23:38:47 +0530

    Update

--- swiftly_backend/swiftly_backend/settings.py
+++ swiftly_backend/swiftly_backend/settings.py
@@ -25,7 +25,6 @@ SECRET_KEY = "django-insecure-7!xzehhllsfqmt(ygt0$-ciu9bl(8fq#xs^-uao(fqo%7$g=&$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
